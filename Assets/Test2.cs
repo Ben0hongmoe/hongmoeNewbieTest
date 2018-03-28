7d96db303acca4d225fb91487a18646fb8ed3604
wenqunmaoX <wenqunmao@hongmoe.com>
2018-03-28 11:56:02 +0800

    添加测试文档33333

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -6,8 +6,9 @@ public class Test2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("测试制造文本冲突。。。。1");
-	}
+        Debug.Log("测试制造5555555555555文本冲突。。。。122222222222222222");
+        Debug.Log("测试制造5555555555555文本冲突。。。。122222222222222222");
+    }
 	
 	// Update is called once per frame
 	void Update () {
