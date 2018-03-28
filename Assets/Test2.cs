726ada999d83159e1fc7207c4cd8a9f0d3153364
wenqunmaoX <wenqunmao@hongmoe.com>
2018-03-28 12:00:14 +0800

    尝试修改冲突。

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -6,7 +6,8 @@ public class Test2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("测试制造文本冲突。。。。1");
+        Debug.Log("测试制造789334410文本冲突。。。。024424244542112211");
+        Debug.Log("测试xxxx");
 	}
 	
 	// Update is called once per frame
